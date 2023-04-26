fbf4c177f6dc99b21f04d38b389873b4f1ad5f4f
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-04-26 18:31:31 -0300

    feat: parte de Edição finalizada

--- Viewer.cs
+++ Viewer.cs
@@ -16,7 +16,31 @@ public class Viewer
 
     public static void Replace(string text)
     {
-        var strong =  new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
-        Console.WriteLine(strong);
+        var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
+        var words = text.Split(' ');
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (strong.IsMatch(words[i]))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(
+                    words[i].Substring(
+                        words[i].IndexOf('>') + 1,
+                        (
+                            (words[i].LastIndexOf('<') - 1) -
+                            words[i].IndexOf('>')
+                        )
+                    )
+                );
+
+                Console.Write(" ");
+            }
+            else
+            {
+                Console.Write(words[i]);
+                Console.Write(" ");
+            }
+        }
     }
 }
