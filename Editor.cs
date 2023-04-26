9cfceb390abe7195e310e23f87a231cef8b69166
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-04-26 17:32:52 -0300

    feat: Menu adicionado

---
+++ Editor.cs
@@ -0,0 +1,15 @@
+
+public static class Editor
+{
+    public static void Show()
+    {
+        Console.Clear();
+
+        Console.WriteLine("MODO EDITOR");
+        Console.WriteLine("-----------");
+    }
+    public static void Start()
+    {
+
+    }
+}
